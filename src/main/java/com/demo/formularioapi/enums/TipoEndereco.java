9a350067bc99699a88ccff38f27f0346969ffeb9
gmozart <mozart.gleison@gmail.com>
2023-02-23 21:53:59 -0300

    Desenvolvida as interfaces das classes

--- src/main/java/com/demo/formularioapi/enums/TipoEndereco.java
+++ src/main/java/com/demo/formularioapi/enums/TipoEndereco.java
@@ -4,6 +4,5 @@ public enum TipoEndereco {
 
     PRINCIPAL,
     SECUNDARIO,
-    CASA,
     TRABALHO;
 }
---
+++ src/main/java/com/demo/formularioapi/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.demo.formularioapi.repository;
+
+import com.demo.formularioapi.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}

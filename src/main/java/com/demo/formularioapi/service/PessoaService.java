95d293e3a76f29db8dd9a62aea8b564349636bb2
gmozart <mozart.gleison@gmail.com>
2023-02-27 13:35:55 -0300

    Recodificando a classe Pessoaservice para tratamento com HttpStatus.

--- src/main/java/com/demo/formularioapi/service/PessoaService.java
+++ src/main/java/com/demo/formularioapi/service/PessoaService.java
@@ -1,13 +1,18 @@
 package com.demo.formularioapi.service;
 
 import com.demo.formularioapi.dto.PessoaDTO;
-import com.demo.formularioapi.entity.Pessoa;
 import com.demo.formularioapi.repository.PessoaRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.exception.GenericJDBCException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.HttpMediaTypeException;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,12 +23,25 @@ public class PessoaService {
 
     private final PessoaRepository pessoaRepository;
 
-    public void save(PessoaDTO pessoaDTO){
-        pessoaRepository.save(PessoaDTO.of(pessoaDTO));
+    public ResponseEntity<?> save(PessoaDTO pessoaDTO){
+        try{
+            return new ResponseEntity<PessoaDTO>(PessoaDTO.of(this.pessoaRepository.save(PessoaDTO.of(pessoaDTO))), HttpStatus.CREATED);
+        } catch (JpaSystemException | GenericJDBCException | DataIntegrityViolationException e){
+            e.printStackTrace();
+            return new ResponseEntity<String>(
+                "Dados inválidos! Verifique se os dados informados já foram cadastrados", HttpStatus.BAD_REQUEST);
+        }
     }
-    public Optional<PessoaDTO> findById(Long id){
-        return PessoaDTO.of(pessoaRepository.findById(id));
+
+    public ResponseEntity<?> findById(Long id){
+            Optional<PessoaDTO> record = PessoaDTO.of(this.pessoaRepository.findById(id));
+            if (record.orElseGet(()-> null) != null){
+                return new ResponseEntity<PessoaDTO>(record.get(), HttpStatus.OK);
+            }else {
+                return new ResponseEntity<String>("Pessoa não localziada!", HttpStatus.BAD_REQUEST);
+            }
     }
+
     public Optional<List<PessoaDTO>> findAll(){
         return Optional.of(PessoaDTO.of(pessoaRepository.findAll()));
     }
